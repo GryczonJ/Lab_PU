b922616fb7103da3b575e89d7b71f727b6c329fa
GryczonJ <jan.gryczon1@gmail.com>
2025-10-08 17:09:06 +0200

    Add logic-mathematical calculator program outline

---
+++ Lab 1/Zadanie 1.py
@@ -0,0 +1,8 @@
+# Zadanie 1: Kalkulator logiczno-matematyczny 
+# Napisz program, który: 
+#  pobiera od użytkownika dwie liczby całkowite, 
+#  wykonuje operacje: dodawanie, odejmowanie, mnożenie, dzielenie, 
+#  sprawdza, czy liczby są równe, czy jedna jest większa od drugiej, 
+#  wyświetla wynik w formacie: „Liczba A jest większa/mniejsza/równa liczbie B”
+
+
