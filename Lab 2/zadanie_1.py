46c0328ad02e7d174745ad6024de687517db8507
GryczonJ <jan.gryczon1@gmail.com>
2025-10-15 18:44:00 +0200

    Add initial implementation of zadanie_1.py with project requirements and instructions

---
+++ Lab 2/zadanie_1.py
@@ -0,0 +1,10 @@
+# Korzystając z modułu Request i bs4.BeautifulSoup odbierz w programie Python ze strony
+# Sejmu RP nazwiska i imiona wszystkich aktualnych posłów na sejm. Zapisz w osobnych
+# kolumnach Pandas DateFrame ich imiona i nazwiska. Następnie korzystając z
+# możliwości DataFrame należy policzyć:,ile jest posłów i posłanek, ilu posłów obojga płci
+# w nazwiskach ma typowo polskie znaki (korzystając z odpowiedniego wyrażenia
+# regularnego i przeznaczonej do tego własnej funkcji polskie_nazwisko, ilu posługuje się
+# wieloczłonowym imieniem, a ilu wieloczłonowym nazwiskiem. Do pliku poslowie.txt
+# proszę zapisać wszystkich posłów oraz na końcu obliczone statystyki. Proszę sprawdzić,
+# czy liczba posłów się zgadza i ewentualnie poprawić kod.
+# W sprawozdaniu proszę umieścić kod poslowie.py oraz wynik poslowie.txt
