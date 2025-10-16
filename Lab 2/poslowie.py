8e43bef3fe83a9bf6589f9dd7b16506513ce0524
GryczonJ <jan.gryczon1@gmail.com>
2025-10-16 19:05:14 +0200

    Refactor gender identification logic and enhance output file with senator statistics

--- Lab 2/poslowie.py
+++ Lab 2/poslowie.py
@@ -32,8 +32,7 @@ wyjądkowe_męskie_imiona = [
 ]
 
 def czy_kobieta(imie: str)->bool:
-    if imie[-1] == "a" or (imie not in wyjądkowe_męskie_imiona) :
-        print(imie)
+    if  imie[-1] == "a" and (imie not in wyjądkowe_męskie_imiona):
         return True
     else: return False
   
@@ -44,26 +43,47 @@ headers = {
     # Opcjonalnie można dodać inne nagłówki, np. Accept-Language
     'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7'
 }
+
 response = req.get(url, headers=headers)
 if(response.status_code != 200):
     print("Błąd połączenia:", response.status_code)
     exit(1)
+
 response.encoding = 'utf-8' # polskie znaki
 html = response.text
 soup = bs(html, 'html.parser')
+
 df = pd.DataFrame(columns=['Imię', 'Nazwisko'])
+
 for kontener in soup.find_all("div",class_="col-lg-4 col-md-4 col-sm-4 col-xs-6"):
     a_tag  = kontener.find("a")
     if a_tag:
         pelne_imie_nazwisko = a_tag.get_text( strip = True )
         imie,nazwisko, = pelne_imie_nazwisko.rsplit(" ", 1) #split(" ", 1)
-        print(f"Imię: {imie}, Nazwisko: {nazwisko}")
+        #print(f"Imię: {imie}, Nazwisko: {nazwisko}")
         df = pd.concat([df, pd.DataFrame({'Imię': [imie], 'Nazwisko': [nazwisko]})], ignore_index=True) 
 
 
 # main
 
-print(df)
-polskie_nazwisko(nazwisko)
+# print(df)
+
+polskich_nazwisk = df['Nazwisko'].apply(polskie_nazwisko)
+posłanki = df ['Imię'].apply(czy_kobieta)
 
 
+WieloImię = df['Imię'].str.contains(' ')
+warunek_spacja = df['Nazwisko'].str.contains(' ')
+warunek_myslnik = df['Nazwisko'].str.contains('-')
+
+# print("nazwiska:", polskich_nazwisk.sum())
+# print("kobiety", posłanki.sum())
+
+# print("dwa imiona:", WieloImię.sum())
+# print("nazwiska", warunek_spacja.sum(),warunek_myslnik.sum())
+
+with open("sentorowie.txt", "w", encoding="utf-8") as file:
+    file.write(f"nazwiska: {polskich_nazwisk.sum()}\n")
+    file.write(f"kobiety: {posłanki.sum()}\n")
+    file.write(f"dwa imiona: {WieloImię.sum()}\n")
+    file.write(f"nazwiska ze spacją: {warunek_spacja.sum()}, nazwiska z myślnikiem: {warunek_myslnik.sum()}\n")
